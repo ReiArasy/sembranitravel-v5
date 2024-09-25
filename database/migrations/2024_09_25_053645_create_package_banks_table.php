e9497a15c3c22cf8033109cfb8e1ea0a2bb6d791
Arasyyygit25 <rifqiarasyy@gmail.com>
2024-09-25 12:43:59 +0700

    category-migration-edit

--- database/migrations/2024_09_25_053645_create_package_banks_table.php
+++ database/migrations/2024_09_25_053645_create_package_banks_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('package_banks', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('icon');
+            $table->string('slug');
             $table->timestamps();
         });
     }
