d6593433d38a9a0bfe5c7bfb1091579c969174c9
Arasyyygit25 <rifqiarasyy@gmail.com>
2024-09-28 20:10:04 +0700

    make seeder

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $permissions = [
+            'manage categories',
+            'manage packages',
+            'manage transactions',
+            
+        ];
+    }
+}
