5f511a8cced9674a5355f67579625ed2bc3dbe66
Arasyyygit25 <rifqiarasyy@gmail.com>
2024-10-02 08:35:02 +0700

    artisan-ORM

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    public function tours(){
+        return $this->hasMany(PackageBooking::class, 'user_id');
+    }
 }
