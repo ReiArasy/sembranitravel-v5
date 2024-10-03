8112e5baf0c5e23caad1ca08aefff5c4b22e4a9b
Arasyyygit25 <rifqiarasyy@gmail.com>
2024-10-03 12:59:30 +0700

    routesweb-add-resource

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +16,29 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('admin')->name('admin.')->group(function () {
+
+        // manage categories
+        Route::middleware('can:manage categories')->group(function () {
+            Route::resource('categories', CategoryController::class);
+        });
+
+        // manage packages
+        Route::middleware('can:manage packages')->group(function () {
+            Route::resource('package_tours', PackageTourController::class);
+        });
+
+        // manage package banks
+        Route::middleware('can:manage package banks')->group(function () {
+            Route::resource('package_banks', PackageBankController::class);
+        });
+
+        // manage transaction
+        Route::middleware('can:manage transaction')->group(function () {
+            Route::resource('package_bookings', PackageBookingController::class);
+        });
+    });
 });
 
 require __DIR__.'/auth.php';
