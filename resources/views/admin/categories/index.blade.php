c69f45b8b234b69f44774329003b1f8b66e8c4cc
Arasyyygit25 <rifqiarasyy@gmail.com>
2024-10-16 23:31:57 +0700

    {{$category->name}}

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -19,8 +19,7 @@
                     <div class="flex flex-row items-center gap-x-3">
                         <img src=" " alt="" class="rounded-2xl object-cover w-[90px] h-[90px]">
                         <div class="flex flex-col">
-                            
-                            <h3 class="text-indigo-950 text-xl font-bold">Name</h3>
+                            <h3 class="text-indigo-950 text-xl font-bold">{{$category->name}}</h3>
                         </div>
                     </div> 
                     <div  class="hidden md:flex flex-col">
