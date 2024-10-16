2bfff5863aa1df187db85c63ca880bb14028bb6d
Arasyyygit25 <rifqiarasyy@gmail.com>
2024-10-16 23:23:33 +0700

    use I\S\F\DB

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 
 
 class CategoryController extends Controller
