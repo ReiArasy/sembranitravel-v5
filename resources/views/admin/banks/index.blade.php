88d1547f06cf59fad8dc0c88de66cba8b28f7243
Arasyyygit25 <rifqiarasyy@gmail.com>
2024-10-26 14:39:23 +0700

    create bank_name view and created at

--- resources/views/admin/banks/index.blade.php
+++ resources/views/admin/banks/index.blade.php
@@ -26,7 +26,9 @@
                     </div> 
                     <div  class="hidden md:flex flex-col">
                         <p class="text-slate-500 text-sm">Date</p>
-                        <h3 class="text-indigo-950 text-xl font-bold">date</h3>
+                        <h3 class="text-indigo-950 text-xl font-bold">
+                            {{$bank->created_at->format('M d, Y')}}
+                        </h3>
                     </div>
                     <div class="hidden md:flex flex-row items-center gap-x-3">
                         <a href=" " class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
