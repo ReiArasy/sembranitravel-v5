90250e879da40f5003bd7e9b28d47a2b4b85bc24
Arasyyygit25 <rifqiarasyy@gmail.com>
2024-11-21 16:55:38 +0700

    bugfixing_updatecategories_onauthcategory_publicfunctionupdateandimporthttprequestupdaecategoryrequest

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreCategoryRequest;
 use App\Models\Category;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\UpdateCategoryRequest;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
