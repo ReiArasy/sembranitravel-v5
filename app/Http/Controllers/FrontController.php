2858bc09b6ba969c5818a2932ccde3ccac24c169
Arasyyygit25 <rifqiarasyy@gmail.com>
2024-12-10 23:24:04 +0700

    function-bookstore-frontcontroller

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePackageBookingRequest;
+use App\Models\PackageBank;
+use App\Models\PackageBooking;
 use App\Models\PackageTour;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FrontController extends Controller
 {
@@ -21,5 +27,44 @@ class FrontController extends Controller
 
     public function book(PackageTour $packageTour){
         return view('front.book', compact('packageTour'));
+    }
+
+    public function book_store(StorePackageBookingRequest $request, PackageTour $packageTour){
+        $user = Auth::user();
+        $bank = PackageBank::orderByDesc('id')->first();
+        $packageBookingid = null;
+
+        DB::transaction(function() use ($request, $user, $packageTour, $bank, &$packageBookingId){
+            $validated = $request->validated();
+
+            $startDate = new Carbon($validated['start_date']);
+            $totalDays = $packageTour->days - 1;
+            // 10, 11, 12, 13, 14
+            $endDate = $startDate->addDays($totalDays);
+
+            $sub_total = $packageTour->price * $validated['quantity'];
+            $insurance = 30000 * $validated['quantity'];
+            $tax = $sub_total * 0.10;
+
+            $validated['end_date'] = $endDate;
+            $validated['user_id'] = $user->id;
+            $validated['is_paid'] = false;
+            $validated['proof'] = 'dummytrx.png';
+            $validated['package_tour_id'] = $packageTour->id;
+            $validated['package_bank_id'] = $bank->id;
+            $validated['insurance'] = $insurance;
+            $validated['tax'] = $tax;
+            $validated['sub_total'] = $sub_total;
+            $validated['total_amount'] = $sub_total + $tax + $insurance;
+
+            $packageBooking = PackageBooking::create($validated);
+            $packageBookingId = $packageBooking->id;            
+        });
+
+        if($packageBookingId){
+            return redirect()->route('front.choose_bank', $packageBookingId);
+        } else {
+            return back()->withErrors('failed to create booking');
+        }
     }
 }
